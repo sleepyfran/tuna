3f49af85ae9557a41df24ec421afec4e82b6723f
sleepyfran <fgzv@outlook.com>
2021-03-27 20:04:24 +0100

    Initial Commit

---
+++ Shared/TunaApp.swift
@@ -0,0 +1,20 @@
+//
+//  TunaApp.swift
+//  Shared
+//
+//  Created by Fran González on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TunaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
